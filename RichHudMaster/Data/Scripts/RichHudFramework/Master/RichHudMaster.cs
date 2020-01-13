ecf46a49f11b6c793bbc1bce02001c07d4657fec
Zach Hembree <6527038+ZachHembree@users.noreply.github.com>
2020-01-13 13:23:52 -0500

    Disabled text editor

--- RichHudMaster/Data/Scripts/RichHudFramework/Master/RichHudMaster.cs
+++ RichHudMaster/Data/Scripts/RichHudFramework/Master/RichHudMaster.cs
@@ -46,12 +46,6 @@ namespace RichHudFramework.Server
 
             RegisterClientHandler();
             CheckClientQueue();
-
-            var testEditor = new TextEditor(HudMain.Root) 
-            { 
-                HeaderText = "Text Editor",
-                Size = new Vector2(500f, 300f),
-            };
         }
 
         private void RegisterClientHandler() =>
