d3c0bb9cdb1782d351b4d536eed8df3467ef4c50
Zach Hembree <zhembree654@gmail.com>
2021-03-27 06:59:29 -0400

    Add plugin entry point for testing

---
+++ RichHudMaster/Data/Scripts/RichHudFramework/Master/RichHudPlugin.cs
@@ -0,0 +1,33 @@
+﻿#if RHM_PLUGIN
+namespace RichHudFramework.Server
+{
+    using RichHudFramework.Internal;
+    using RichHudFramework.IO;
+    using RichHudFramework.UI;
+    using RichHudFramework.UI.Rendering;
+    using Sandbox.ModAPI;
+    using System;
+    using System.Collections.Generic;
+    using VRage;
+    using VRage.Game;
+    using VRage.Game.Components;
+    using VRage.Game.ModAPI;
+    using VRageMath;
+    using VRage.Plugins;
+
+    /// <summary>
+    /// Plugin entry point used for testing
+    /// </summary>
+    public class RichHudMasterPlugin : IPlugin
+    {
+        public void Dispose()
+        { }
+
+        public void Init(object gameInstance)
+        { }
+
+        public void Update()
+        { }
+    }
+}
+#endif
