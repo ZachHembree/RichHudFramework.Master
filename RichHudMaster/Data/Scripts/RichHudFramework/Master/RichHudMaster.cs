7d35c32152a48525c28c2371c42f06ff4fda45cb
Zach Hembree <zhembree654@gmail.com>
2022-02-18 06:21:25 -0500

    Increment VID

--- RichHudMaster/Data/Scripts/RichHudFramework/Master/RichHudMaster.cs
+++ RichHudMaster/Data/Scripts/RichHudFramework/Master/RichHudMaster.cs
@@ -28,7 +28,7 @@ namespace RichHudFramework.Server
     {
         public const long modID = 1965654081, queueID = 1314086443;
         public const int apiVID = 10, minApiVID = 7;
-        public static readonly Vector4I versionID = new Vector4I(1, 2, 1, 0); // Major, Minor, Rev, Hotfix
+        public static readonly Vector4I versionID = new Vector4I(1, 2, 2, 0); // Major, Minor, Rev, Hotfix
 
         /// <summary>
         /// Read-only list of currently registered clients
